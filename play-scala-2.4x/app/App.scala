c2a6e7616333c030e5c42ec9bf7699be38af8935
itang <live.tang@gmail.com>
2015-03-16 16:35:19 +0800

    more

--- play-scala-2.4x/app/App.scala
+++ play-scala-2.4x/app/App.scala
@@ -4,14 +4,23 @@ import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
 
 object XRuntimeFilter extends Filter {
+
+  val XRuntimeRespKey = "X-Runtime"
+
   def apply(next: (RequestHeader) => Future[Result])(request: RequestHeader): Future[Result] = {
     val s = System.nanoTime()
     val result = next(request).map { response =>
       val e = System.nanoTime()
-      response.withHeaders("XRuntime" -> "%.4fms".format((e - s) / (1000.0 * 1000)))
+      response.withHeaders(XRuntimeRespKey -> formatCostTime(s, e))
     }
     result
   }
+
+  private def formatCostTime(start: Long, end: Long) = {
+    val costInMs = (end - start) / (1000.0 * 1000)
+    "%.4fms".format(costInMs)
+  }
+
 }
 
 object Global extends WithFilters(XRuntimeFilter)
--- play-scala-2.4x/app/controllers/Application.scala
+++ play-scala-2.4x/app/controllers/Application.scala
@@ -3,17 +3,22 @@ package controllers
 import play.api._
 import play.api.mvc._
 
+import play.api.libs.json._
+
 object Application extends Controller {
 
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
   }
-  
+
   def ping = Action {
     Ok("pong")
   }
-  
-  def test = Action {
-    Ok("Hello")
+
+  def echoJSON = Action(parse.json) { implicit request =>
+    //println(s"request.body[${request.body.getClass}]")
+    Ok(Json.obj("now" -> JsNumber((new java.util.Date).getTime),
+                "data" -> request.body))
+      .as(JSON)
   }
 }
